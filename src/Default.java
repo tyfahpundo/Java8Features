31f0ad665bb3dabd1792d7911a1a05aeca92806a
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-10-09 01:37:16 +0200

    Override The default method

--- src/Default.java
+++ src/Default.java
@@ -4,6 +4,9 @@ public interface Default {
     }
 }
 class Demoo implements Default{
+    public void mi(){
+        System.out.println("My own Implementation");
+    }
     public static void main(String[] args) {
         Demoo demo = new Demoo();
         demo.mi();
