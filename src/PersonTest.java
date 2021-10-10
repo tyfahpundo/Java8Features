3e3b20bf0dcffea39953142b6812a73cfe26ec40
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-10-10 14:28:56 +0200

    Used Function and Predicate in a program

--- src/PersonTest.java
+++ src/PersonTest.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class PersonTest {
     public static void main(String[] args) {
@@ -12,17 +13,31 @@ public class PersonTest {
           }
           return total;
         };
-        System.out.println("The toatl monthly salary amounts to: "+ fn.apply(personList));
+        System.out.println("The total monthly salary amounts to: "+ fn.apply(personList));
+        Predicate<Person> p = person->person.salary<30000;
+        Function<Person,Person> f = p1->{
+            p1.salary +=447;
+            return p1;
+        };
+        ArrayList<Person> list2 = new ArrayList<>();
+        for(Person person: personList){
+            if(p.test(person)){
+                f.apply(person);
+                list2.add(person);
+            }
+        }
+        System.out.println("After Increments");
+        System.out.println(list2);
     }
 
     private static void populate(ArrayList<Person> personList) {
         personList.add(new Person("Tafadzwa",20000));
-        personList.add(new Person("Tafadzwa",40000));
-        personList.add(new Person("Tafadzwa",90000));
-        personList.add(new Person("Tafadzwa",22900));
-        personList.add(new Person("Tafadzwa",14300));
-        personList.add(new Person("Tafadzwa",34510));
-        personList.add(new Person("Tafadzwa",54237));
-        personList.add(new Person("Tafadzwa",67435));
+        personList.add(new Person("Blessing",40000));
+        personList.add(new Person("Rodney",90000));
+        personList.add(new Person("Tadiwa",22900));
+        personList.add(new Person("Monalisah",14300));
+        personList.add(new Person("Allen",34510));
+        personList.add(new Person("Alex",54237));
+        personList.add(new Person("Lynn",67435));
     }
 }
