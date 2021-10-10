50ab09480ca47e113bbf01624ff502c52a3d23ab
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-10-10 08:47:59 +0200

    Learning about Function functional Interface

---
+++ src/FunctionInt.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class FunctionInt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String and l will give you its length: ");
+        String name = sc.next();
+
+        Function<String,Integer> f = s->s.length();
+        System.out.println(f.apply(name));
+        System.out.println("*********************************************************");
+        System.out.println("The square of the Provided Number: ");
+
+        Function<Integer,Integer>fn = i->i*i;
+        System.out.println("Please enter the number you want to square");
+        int number = sc.nextInt();
+        System.out.println(fn.apply(number));
+    }
+}
