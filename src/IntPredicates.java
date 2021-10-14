a5dae0db34bf7902861eb210ff71b31d504b8794
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-10-14 12:12:07 +0200

    Unary Operator is a Child of Function

--- src/IntPredicates.java
+++ src/IntPredicates.java
@@ -1,4 +1,7 @@
 import java.util.function.IntPredicate;
+import java.util.function.IntSupplier;
+import java.util.function.ToIntFunction;
+import java.util.function.UnaryOperator;
 
 public class IntPredicates {
     public static void main(String[] args) {
@@ -9,5 +12,16 @@ public class IntPredicates {
                 System.out.println(a);
             }
         }
+        ToIntFunction<String> f = s->s.length();
+        System.out.println(f.applyAsInt("tyfah"));
+
+        IntSupplier s =()-> (int)(Math.random()*10);
+        String otp ="";
+        for(int i=0;i<6;i++){
+            otp = otp+s.getAsInt();
+        }
+        System.out.println(otp);
+        UnaryOperator<Integer> o = i->i*i;
+        System.out.println(o.apply(23));
     }
 }
