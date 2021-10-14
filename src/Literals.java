120f270513e5b193d7752d0451092848c0bd9223
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-10-14 08:33:47 +0200

    Literals

--- src/Literals.java
+++ src/Literals.java
@@ -3,6 +3,11 @@ public class Literals {
         int x = 10;
         int y = 010;
         int z =0x10;
+        double d = 12e3;
+
+        Integer I = 10;
+        System.out.println(I);
+        System.out.println(d);
         System.out.println(x+"..."+y+"..."+z);
     }
 }
